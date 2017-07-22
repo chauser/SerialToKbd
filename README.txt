7009e9e08272bf6acdc00d35d36a79013e3446ac
Carl Hauser <carl.hauser@gmail.com>
2017-07-22 09:14:57 -0700

    First commit of actual source files

---
+++ README.txt
@@ -0,0 +1,49 @@
+SerialToKbd
+
+SerialToKbd is a simple helper program that allows MagTek USB check/credit card
+readers 22523003 and 22533007 to be used with software that doesn't specifically support check readers.
+It is intended as a replacement for the MicrSend program that MagTek used to provide but which no
+longer works with recent versions of Windows.
+
+You do NOT need this program if:
+    Your app supports the check reader directly (for example PowerChurch running locally)
+    You have a USB MiniMicr that does keyboard emulation (part number 22523009 and similar)
+    You have a keyboard "wedge" MiniMicr that plugs into the PS/2 keyboard port of your computer
+
+This program MAY help if you have non-keyboard-emulation USB MiniMicr or a MiniMicr that plugs into
+the serial port on your computer AND your app doesn't support the MiniMicr directly (for example
+PowerChurch online) (tested with 22533007 readers).
+
+Before using SerialToKbd you should install the MagTek drivers for your device.
+
+The MiniMicr can format its output in a large number of different ways. The app you intend to use it with
+probably requires a specific format. The can be set up using the MicrBase program from MagTek's web site.
+Making sure that MicrBase works also helps ensure that all the prerequisites for using SerialToKbd have
+been met.
+
+Use: launch SerialToKbd by double-clicking it. It open a console window and tell you what it is doing.
+
+If it does not find your MiniMicr: use the Windows Device Manager to look at the Ports (COM & LPT) on your computer.
+The USB Mini Micr should be listed as such. A MiniMicr attached with a serial cable may not be. In this case
+try launching SerialToKbd from a command prompt and passing the COM port name (e.g. COM1) as a parameter:
+    SerialToKbd COM1
+
+You can test whether the MiniMicr, SerialToKbd, and other apps on your computer are working by
+opening NotePad or WordPad and placing the cursor in it. Then scan a check: you should see the check
+information appear as characters in the document.
+
+I would be interested in learning the MagTek part numbers of additional readers that this program
+can help with as well as their USB Vendor and Product IDs. The Vendor and Product IDs can be found
+using the Windows Device Manager. The part number - something like 22xyyzzzz -
+can be found on a label on the bottom of the reader.
+
+Please post information as an "Issue" on the project github page:
+    http://github.com/chauser/SerialToKbd
+
+This program is derived from pieces of the open source stenography Plover project that turned out to have all the functionality
+needed to implement a replacement for MicrSend. Thanks to the Plover creators for their work!
+
+License: see License.txt
+
+Source code for the program may be found at the project github page:
+    http://github.com/chauser/SerialToKbd
---
+++ SerialToKbd.py
@@ -0,0 +1,220 @@
+# Copyright (c) 2011 Hesky Fisher.
+# Copyright (c) 2017 Carl Hauser.
+# See LICENSE.txt for details.
+#
+# This file is derived from winkeyboardcontrol.py - capturing and
+# injecting keyboard events in windows - from version 3 of Plover, the open source
+# steno project.
+# I (Carl Hauser) have changed it to serve as a standalone forwarder of characters
+# from a serial port to the keyboard input of the currently in-focus window
+
+"""Keyboard capture and control in windows.
+
+This module provides an interface for basic keyboard event capture and
+emulation. Set the key_up and key_down functions of the
+KeyboardCapture class to capture keyboard input. Call the send_string
+and send_backspaces functions of the KeyboardEmulation class to
+emulate keyboard input.
+
+"""
+
+import ctypes
+import multiprocessing
+import os
+import threading
+
+from ctypes import windll, wintypes
+
+# Python 2/3 compatibility.
+from keyboardlayout import KeyboardLayout
+
+SendInput = windll.user32.SendInput
+LONG = ctypes.c_long
+DWORD = ctypes.c_ulong
+ULONG_PTR = ctypes.POINTER(DWORD)
+WORD = ctypes.c_ushort
+KEYEVENTF_EXTENDEDKEY = 0x0001
+KEYEVENTF_KEYUP = 0x0002
+KEYEVENTF_UNICODE = 0x0004
+INPUT_MOUSE = 0
+INPUT_KEYBOARD = 1
+
+class MOUSEINPUT(ctypes.Structure):
+    _fields_ = (('dx', LONG),
+                ('dy', LONG),
+                ('mouseData', DWORD),
+                ('dwFlags', DWORD),
+                ('time', DWORD),
+                ('dwExtraInfo', ULONG_PTR))
+
+class KEYBDINPUT(ctypes.Structure):
+    _fields_ = (('wVk', WORD),
+                ('wScan', WORD),
+                ('dwFlags', DWORD),
+                ('time', DWORD),
+                ('dwExtraInfo', ULONG_PTR))
+
+class _INPUTunion(ctypes.Union):
+    _fields_ = (('mi', MOUSEINPUT),
+                ('ki', KEYBDINPUT))
+
+class INPUT(ctypes.Structure):
+    _fields_ = (('type', DWORD),
+                ('union', _INPUTunion))
+
+class KeyboardEmulation(object):
+
+    def __init__(self):
+        self.keyboard_layout = KeyboardLayout()
+
+    # Sends input types to buffer
+    @staticmethod
+    def _send_input(*inputs):
+        len_inputs = len(inputs)
+        len_pinput = INPUT * len_inputs
+        pinputs = len_pinput(*inputs)
+        c_size = ctypes.c_int(ctypes.sizeof(INPUT))
+        return SendInput(len_inputs, pinputs, c_size)
+
+    # Input type must be keyboard
+    @staticmethod
+    def _input(structure):
+        if isinstance(structure, KEYBDINPUT):
+            return INPUT(INPUT_KEYBOARD, _INPUTunion(ki=structure))
+        raise TypeError('Cannot create INPUT structure!')
+
+    # Keyboard input type to send key input
+    @staticmethod
+    def _keyboard_input(code, flags):
+        if flags == KEYEVENTF_UNICODE:
+            # special handling of Unicode characters
+            return KEYBDINPUT(0, code, flags, 0, None)
+        return KEYBDINPUT(code, 0, flags, 0, None)
+
+    # Abstraction to set flags to 0 and create an input type
+    def _keyboard(self, code, flags=0):
+        return self._input(self._keyboard_input(code, flags))
+
+    def _key_event(self, keycode, pressed):
+        flags = 0 if pressed else KEYEVENTF_KEYUP
+        self._send_input(self._keyboard(keycode, flags))
+
+    # Press and release a key
+    def _key_press(self, char):
+        vk, ss = self.keyboard_layout.char_to_vk_ss[char]
+        keycode_list = []
+        keycode_list.extend(self.keyboard_layout.ss_to_vks[ss])
+        keycode_list.append(vk)
+        # Press and release all keys.
+        for keycode in keycode_list:
+            self._key_event(keycode, True)
+            self._key_event(keycode, False)
+
+    def _refresh_keyboard_layout(self):
+        layout_id = KeyboardLayout.current_layout_id()
+        if layout_id != self.keyboard_layout.layout_id:
+            self.keyboard_layout = KeyboardLayout(layout_id)
+
+    def _key_unicode(self, char):
+        inputs = [self._keyboard(ord(code), KEYEVENTF_UNICODE)
+                  for code in char]
+        self._send_input(*inputs)
+
+    def send_string(self, s):
+        self._refresh_keyboard_layout()
+        for char in s:
+            if char in self.keyboard_layout.char_to_vk_ss:
+                # We know how to simulate the character.
+                self._key_press(char)
+            else:
+                # Otherwise, we send it as a Unicode string.
+                self._key_unicode(char)
+
+    def send_key_combination(self, combo_string):
+        """Emulate a sequence of key combinations.
+        Argument:
+        combo_string -- A string representing a sequence of key
+        combinations. Keys are represented by their names in the
+        self.keyboard_layout.keyname_to_keycode above. For example, the
+        left Alt key is represented by 'Alt_L'. Keys are either
+        separated by a space or a left or right parenthesis.
+        Parentheses must be properly formed in pairs and may be
+        nested. A key immediately followed by a parenthetical
+        indicates that the key is pressed down while all keys enclosed
+        in the parenthetical are pressed and released in turn. For
+        example, Alt_L(Tab) means to hold the left Alt key down, press
+        and release the Tab key, and then release the left Alt key.
+        """
+        # Make sure keyboard layout is up-to-date.
+        self._refresh_keyboard_layout()
+        # Parse and validate combo.
+        key_events = parse_key_combo(combo_string, self.keyboard_layout.keyname_to_vk.get)
+        # Send events...
+        for keycode, pressed in key_events:
+            self._key_event(keycode, pressed)
+
+
+def get_option_info(port):
+    """Get the default options for this machine."""
+    return {
+            'port': (port), 
+            'baudrate': (9600),
+            'bytesize': (8),
+            'parity': ('N'),
+            'stopbits': (1),
+            'timeout': (2.0),
+            'xonxoff': (False),
+            'rtscts': (False)
+           }
+
+import serial
+def start_capture(port):
+        serial_params = get_option_info(port)
+        try:
+            serial_port = serial.Serial(**serial_params)
+        except (serial.SerialException, OSError):
+            print ('Can\'t open serial port: %s' % port)
+            return
+
+        if not serial_port.isOpen():
+            print('Serial port is not open: %s' & self.serial_params.get('port'))
+            return
+
+        kbd_emulator = KeyboardEmulation()
+        try:
+            while True:
+
+                # Grab data from the serial port.
+                raw = serial_port.read(1)
+                if not raw:
+                    continue
+
+                # Send the character view the keyboard emulator
+                kbd_emulator.send_string(chr(raw[0]))
+        except KeyboardInterrupt:
+            pass
+                
+import serial.tools.list_ports as LP
+import sys
+device = None
+if len(sys.argv) > 1:
+    device = sys.argv[1]
+else:
+    print ("Looking for a Magtek reader")
+    ports = LP.comports()
+    for p in ports:
+        if p.manufacturer=='MagTek':
+           print("Found a MagTek reader with Vendor ID: 0x%x and Product ID: 0x%x on %s" %
+                 (p.vid, p.pid, p.device))
+           device=p.device
+if device:
+    print("Opening reader on %s" % device)
+    print("Type Ctrl-C or close this window to exit")
+    start_capture(device)
+else:
+    print("Did not find a supported reader; Use the device manager to identify the COM port")
+    print("and then enter the command")
+    print("    SerialToKbd COMn")
+    print("where n is the number of the COM port you identified in the device manager")
+    sys.stdin.readline()
+
